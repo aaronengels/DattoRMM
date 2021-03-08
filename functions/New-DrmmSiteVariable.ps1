2d2d6d1f83337463acd13540d04e5d79a5324554
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2021-03-08 09:43:56 -0900

    Add functions to control site variables
    Functions to create, read, update, and delete site variables

---
+++ functions/New-DrmmSiteVariable.ps1
@@ -0,0 +1,52 @@
+function New-DrmmSiteVariable {
+
+	<#
+	.SYNOPSIS
+	Creates a new site variable with the given name and value for the given site. The value can optionally be masked.
+
+    .PARAMETER siteUid
+    The UID of the site under which this variable should be placed.
+    
+	.PARAMETER name
+	The name of the variable.
+
+    .PARAMETER value
+	The value of the variable.
+
+    .PARAMETER masked
+	Switch to set whether the value should be masked.
+	
+	#>
+    
+	# Function Parameters
+    Param (
+        [Parameter(Mandatory=$True)] 
+        $siteUid,
+        [Parameter(Mandatory=$True)] 
+        $name,
+        [Parameter(Mandatory=$True)] 
+        $value,
+        [Parameter(Mandatory=$false)]
+        [Switch]
+        $masked
+    )
+	
+    # Declare Variables
+    $apiMethod = 'PUT'
+	$createVariableRequest = @{}
+    
+    $createVariableRequest.Add('name',$name)
+    $createVariableRequest.Add('value',$value)
+    If ( $masked -eq $true ) {
+        $createVariableRequest.Add('masked',$true)
+    } Else {
+        $createVariableRequest.Add('masked',$false)
+    }
+
+    # Convert to JSON
+	$Body = $createVariableRequest | ConvertTo-Json
+
+	# Return Variable
+    return New-ApiRequest -apiMethod $apiMethod -apiRequest "/v2/site/$siteUid/variable" -apiRequestBody $Body | ConvertFrom-Json
+
+}
---
+++ functions/Remove-DrmmSiteVariable.ps1
@@ -0,0 +1,30 @@
+function Remove-DrmmSiteVariable {
+
+	<#
+	.SYNOPSIS
+	Removes a site variable with the given variableId under the site with the given site UID.
+
+    .PARAMETER siteUid
+    The UID of the site under which the variable exists.
+    
+	.PARAMETER variableId
+	The name of the variable.
+	
+	#>
+    
+	# Function Parameters
+    Param (
+        [Parameter(Mandatory=$True)] 
+        $siteUid,
+        
+        [Parameter(Mandatory=$True)] 
+        $variableId
+    )
+	
+    # Declare Variables
+    $apiMethod = 'DELETE'
+
+	# Return Variable
+    return New-ApiRequest -apiMethod $apiMethod -apiRequest "/v2/site/$siteUid/variable/$variableId" | ConvertFrom-Json
+
+}
