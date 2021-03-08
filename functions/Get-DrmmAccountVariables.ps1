7d89edd2341970bdb928fa56241863cf84871094
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2021-03-08 09:43:33 -0900

    Add functions to control account variables
    Functions to create, read, update, and delete account variables

---
+++ functions/Get-DrmmAccountVariables.ps1
@@ -0,0 +1,28 @@
+function Get-DrmmAccountVariables {
+
+	<#
+	.SYNOPSIS
+	Gets account variables.
+	
+	#>
+	
+    # Declare Variables
+    $apiMethod = 'GET'
+    $maxPage = 250
+    $nextPageUrl = $null
+    $page = 0
+    $Results = @()
+
+    do {
+        $Response = New-ApiRequest -apiMethod $apiMethod -apiRequest "/v2/account/variables?max=$maxPage&page=$page" | ConvertFrom-Json
+	    if ($Response) {
+		    $nextPageUrl = $Response.pageDetails.nextPageUrl
+		    $Results += $Response.variables
+		    $page++
+	    }
+    }
+    until ($null -eq $nextPageUrl)
+
+    # Return all account devices
+    return $Results
+}
---
+++ functions/New-DrmmAccountVariable.ps1
@@ -0,0 +1,47 @@
+function New-DrmmAccountVariable {
+
+	<#
+	.SYNOPSIS
+	Creates a new account variable with the given name and value. The value can optionally be masked.
+
+	.PARAMETER name
+	The name of the variable.
+
+    .PARAMETER value
+	The value of the variable.
+
+    .PARAMETER masked
+	Switch to set whether the value should be masked.
+	
+	#>
+    
+	# Function Parameters
+    Param (
+        [Parameter(Mandatory=$True)] 
+        $name,
+        [Parameter(Mandatory=$True)] 
+        $value,
+        [Parameter(Mandatory=$false)]
+        [Switch]
+        $masked
+    )
+	
+    # Declare Variables
+    $apiMethod = 'PUT'
+	$createVariableRequest = @{}
+    
+    $createVariableRequest.Add('name',$name)
+    $createVariableRequest.Add('value',$value)
+    If ( $masked -eq $true ) {
+        $createVariableRequest.Add('masked',$true)
+    } Else {
+        $createVariableRequest.Add('masked',$false)
+    }
+
+    # Convert to JSON
+	$Body = $createVariableRequest | ConvertTo-Json
+
+	# Return Variable
+    return New-ApiRequest -apiMethod $apiMethod -apiRequest "/v2/account/variable" -apiRequestBody $Body | ConvertFrom-Json
+
+}
