a43e47517b1e34c18752cadc283cb45d24f9e4d4
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2021-03-23 17:35:55 -0800

    Update Set-DrmmDeviceQuickJob.ps1
    I was never able to get a hashtable that would properly set multiple variable values.
    
    Added code to convert a simple hashtable, e.g. @{"bar"="baz";"qux"="quux"}, into an array that is compatible with Datto RMM API.
    
    Added example call to make it clear how to specify variables.

--- functions/Set-DrmmDeviceQuickJob.ps1
+++ functions/Set-DrmmDeviceQuickJob.ps1
@@ -19,6 +19,8 @@ function Set-DrmmDeviceQuickJob {
 	.PARAMETER Variables 
 	Provide variables names and values, must be a hash.
 
+	.EXAMPLE
+    $response = Set-DrmmDeviceQuickJob -DeviceUid '00000000-0000-0000-0000-000000000000' -jobName "Quick Job running Foo" -ComponentName "Foo" -Variables @{"bar"="baz";"qux"="quux"}
 	#>
 
 	# Function Parameters
@@ -33,7 +35,7 @@ function Set-DrmmDeviceQuickJob {
         $ComponentName,
 
         [Parameter(Mandatory=$False)] 
-        $variables
+        [hashtable]$variables
 
     )
 	
@@ -55,14 +57,24 @@ function Set-DrmmDeviceQuickJob {
 		throw "Could not find a component named `"$ComponentName`" specified by parameter 'ComponentName'"
 	}
 
+	#convert variable data from hashtable to array that can be converted to API-compatible JSON object
+    $variablesArray = @()
+    foreach ( $Key in $variables.Keys ) {
+        $temp = @{
+            "name" = $Key
+            "value" = $variables[$Key]
+        }
+        $variablesArray += $temp
+    }
+
 	# Create quick job request
 	$quickJobRequest.Add('jobName',$jobName)
 	$jobComponent.Add('componentUid',$componentUid)
-	$jobComponent.Add('variables',$variables)
+	$jobComponent.Add('variables',$variablesArray)
 	$quickJobRequest.Add('jobComponent',$jobComponent)
 
 	# Convert to JSON
-	$Body = $quickJobRequest | ConvertTo-Json
+	$Body = $quickJobRequest | ConvertTo-Json -Depth 3
 
 	# Update UDFs
 	return New-ApiRequest -apiMethod $apiMethod -apiRequest "/v2/device/$deviceUid/quickjob" -apiRequestBody $Body | ConvertFrom-Json
