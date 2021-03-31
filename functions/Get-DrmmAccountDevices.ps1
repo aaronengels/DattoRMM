8454009b40cdbc940c4f89cfe066222bf729f770
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2021-03-31 08:59:25 -0800

    Update Get-DrmmAccountDevices.ps1
    Add optional FilterId parameter added in Datto RMM API 9.3.0

--- functions/Get-DrmmAccountDevices.ps1
+++ functions/Get-DrmmAccountDevices.ps1
@@ -6,18 +6,29 @@
 
 	.DESCRIPTION
 	Returns device data, including patch status, anti-virus status and user defined fields.
-
+	
+	.PARAMETER FilterId
+	Optional parameter specifying a filter to return only some devices 
 	#>
+
+	# Function Parameters
+	Param (
+		[Parameter(Mandatory=$False)]
+		[String]$FilterId
+	)
 
 	# Declare Variables
 	$apiMethod = 'GET'
 	$maxPage = 250
 	$nextPageUrl = $null
 	$page = 0
+	if ( $null -ne $FilterId ) {
+		$filterQuery = "&filterId=$FilterId"
+	}
 	$Results = @()
 
 	$Results = do {
-		$Response = New-ApiRequest -apiMethod $apiMethod -apiRequest "/v2/account/devices?max=$maxPage&page=$page" | ConvertFrom-Json
+		$Response = New-ApiRequest -apiMethod $apiMethod -apiRequest "/v2/account/devices?max=$maxPage&page=$page$filterQuery" | ConvertFrom-Json
 		if ($Response) {
 			$nextPageUrl = $Response.pageDetails.nextPageUrl
 			$Response.devices
