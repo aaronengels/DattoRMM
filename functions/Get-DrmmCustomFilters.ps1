4cc195bc7ae26966bb29d6067f5b13b8707e34a8
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2021-03-31 09:00:25 -0800

    Create Get-DrmmCustomFilters.ps1
    Added function to access new endpoint added in Datto RMM API release 9.3.0

---
+++ functions/Get-DrmmCustomFilters.ps1
@@ -0,0 +1,33 @@
+function Get-DrmmCustomFilters {
+
+	<#
+	.SYNOPSIS
+	Fetches custom device filters.
+
+	.DESCRIPTION
+	Returns all custom device filters that are viewable by the account as administrator.
+
+	#>
+    
+	# Declare Variables
+    $apiMethod = 'GET'
+
+    # Declare Variables
+    $apiMethod = 'GET'
+    $maxPage = 250
+    $nextPageUrl = $null
+    $page = 0
+    $Results = @()
+
+    $results = do {
+        $Response = New-ApiRequest -apiMethod $apiMethod -apiRequest "/v2/filter/custom-filters?max=$maxPage&page=$page" | ConvertFrom-Json
+        if ($Response) {
+            $nextPageUrl = $Response.pageDetails.nextPageUrl
+            $Response.filters
+            $page++
+        }
+    }
+    until ($null -eq $nextPageUrl)
+	return $results
+
+}
